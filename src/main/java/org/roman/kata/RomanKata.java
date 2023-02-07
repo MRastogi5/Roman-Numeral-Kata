26b9172e23130f6268948dde9dbe06a238bfc09e
MRastogi5 <118384562+MRastogi5@users.noreply.github.com>
2023-02-07 23:48:50 +0000

    Updated RomanKata Class to pass failed tests

--- src/main/java/org/roman/kata/RomanKata.java
+++ src/main/java/org/roman/kata/RomanKata.java
@@ -2,19 +2,24 @@ package org.roman.kata;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class RomanKata {
 
-    public String convertToRoman(int integerValue) {
-        Map<Integer, String> map = new HashMap<>();
-        map.put(1, "I");
-        map.put(5, "V");
-        map.put(10, "X");
-        map.put(50, "L");
-        map.put(100, "C");
-        map.put(500,"D")
-        map.put(1000, "M");
+    public String convertToRoman(int intValue) { //200 CC
 
-        return map.get(integerValue);
+        int[] values = {1000, 500, 100, 50, 10, 5, 1}; // int values
+        char[] romanLiterals = {'M', 'D', 'C', 'L', 'X', 'V', 'I'}; // respective roman character
+
+
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < values.length; i++) { // loop through int array to get the max roman character
+            while (intValue >= values[i]) {  // 200>1000; 200>500; 200>100 = C
+                intValue -= values[i]; // intValue = 200 - 100 = 100
+                s.append(romanLiterals[i]); // C 
+            }
+        }
+
+        return s.toString();
     }
 }
