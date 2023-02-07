e6e00424b8cf43551e7e609919c0f5502f60af13
MRastogi5 <118384562+MRastogi5@users.noreply.github.com>
2023-02-07 23:06:46 +0000

    Updated RomanKata class to pass test

--- src/main/java/org/roman/kata/RomanKata.java
+++ src/main/java/org/roman/kata/RomanKata.java
@@ -1,5 +1,19 @@
 package org.roman.kata;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanKata {
 
+    public String convertToRoman(int integerValue) {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "I");
+        map.put(5, "V");
+        map.put(10, "X");
+        map.put(50, "L");
+        map.put(100, "C");
+        map.put(1000, "M");
+
+        return map.get(integerValue);
+    }
 }
